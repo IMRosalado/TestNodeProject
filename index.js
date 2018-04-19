7c4cf0852aebed4fc467254fbcded9071db90bb9
Issa <imrosalado@gmail.com>
2018-04-19 14:37:36 +0800

    g

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 #! /usr/bin/env node
 
 var chalk = require('chalk')
-var git = require("nodegit")
+var git = require("nodegit-kit")
 
 console.log(chalk.green("Hello sworld"))
 
-git.Repository.open("../Test NPM")
+git.open("../Test NPM")
     .then(function(repo){
-        git.Diff(repo, 
-            "2e98d82bab97c6b7f419e182f74099dc53f8cc4b",
-            "2e98d82bab97c6b7f419e182f74099dc53f8cc4b")
+        return git.log(repo, { sort: 'reverse' })
+            .then(function(history){
+                var commit1 = history[0].commit;
+                var commit2 = history[1].commit;
+                // git diff <from> <to>
+                return git.diff(repo, commit1, commit2);
+            })
+            .then(function(diff){
+                console.log(diff);
+            });
     });
 
 
