717afb2036ec4e1d4da4fdeb8eaedd990a32117a
Issa <imrosalado@gmail.com>
2018-04-19 14:16:51 +0800

    commit 2

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 #! /usr/bin/env node
 
 var chalk = require('chalk')
-var git = require("simple-git")
+var git = require("nodegit")
 
-console.log(chalk.green("Hello world"))
+console.log(chalk.green("Hello sworld"))
+
+git.Repository.open("../Test NPM")
+    .then(function(repo){
+        git.Diff(repo, 
+            "2e98d82bab97c6b7f419e182f74099dc53f8cc4b",
+            "2e98d82bab97c6b7f419e182f74099dc53f8cc4b")
+    });
 
 
-console.log(git)
 
