746c1c541bea2d2da0b4193bd63eac8703176c83
Issa <imrosalado@gmail.com>
2018-05-02 13:21:43 +0800

    remove node_modules

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 #! /usr/bin/env node
 
+
 var chalk = require('chalk')
-var git = require("nodegit-kit")
+var cmd = require('node-cmd');
+var Promise = require('bluebird')
 
-console.log(chalk.green("Hello sworld"))
+// cmd.get(`cd ..\\ & git status
+// `, function(err,data,stderr){
+//     console.log(chalk.red(err));
+//     console.log(chalk.green(data));
+//     console.log(chalk.yellow(stderr));
+// }).on('close', function(){
 
-git.open("../Test NPM")
-    .then(function(repo){
-        return git.log(repo, { sort: 'reverse' })
-            .then(function(history){
-                var commit1 = history[0].commit;
-                var commit2 = history[1].commit;
-                // git diff <from> <to>
-                return git.diff(repo, commit1, commit2);
-            })
-            .then(function(diff){
-                console.log(diff);
-            });
-    });
+// console.log(chalk.blue("-----------END-------------"));
+// })
+// console.log(chalk.cyan("------------------------"));
 
+
+
+const getAsync = Promise.promisify(cmd.get, { multiArgs: true, context: cmd })
+ 
+getAsync('git --git-dir=.gitTwo diff --name-only 2e98d82bab97c6b7f419e182f74099dc53f8cc4b').then(function(data){
+    
+    console.log(decodeURI(data[0]))
+}).catch(err => {
+  console.log('cmd err', err)
+})
 
 
